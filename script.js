9a30891262fcd481f6e5acc1c982356f97624258
Megan Bryan <meganmbryan12@gmail.com>
2020-12-10 11:25:08 -0800

    Add local storage function for saved schedule items

--- script.js
+++ script.js
@@ -3,6 +3,20 @@ var currentHour = parseInt (dayjs().format("HH"))
 var currentDay = dayjs().format(`MMMM D, YYYY`)
 $("#currentDay").text(currentDay)
 var textContent = $(".textArea")
+var saveButton = $("#save-button")
+
+function storeText () {
+    for (var i = 1; i < 10; i++) {
+        var textArea = $(`#text-area-${i}`)
+        var savedText = localStorage.getItem(`textInput${i}`, JSON.stringify(textArea.val()))
+        textArea.text(savedText)
+    }
+    
+    $("button").click(function() {
+        var buttonNumber = $(this).val()
+        localStorage.setItem(`textInput${buttonNumber}`, $(`#text-area-${buttonNumber}`).val())
+    })
+}
 
 for (var i=0; i < textContent.length; i++) {
     var j = 9
