7498161ee6cbe7dc9cc786c80d8a693f4e7e7a8c
Megan Bryan <meganmbryan12@gmail.com>
2020-12-10 11:24:06 -0800

    Add ability to color the time blocks by past, present, future

--- script.js
+++ script.js
@@ -2,3 +2,20 @@ var date = dayjs().format()
 var currentHour = parseInt (dayjs().format("HH"))
 var currentDay = dayjs().format(`MMMM D, YYYY`)
 $("#currentDay").text(currentDay)
+var textContent = $(".textArea")
+
+for (var i=0; i < textContent.length; i++) {
+    var j = 9
+    var hourNumber = i + j 
+    if (hourNumber < currentHour) {
+        textContent[i].setAttribute("class", "col-10 textArea row past")
+    }
+    else if (hourNumber === currentHour) {
+        textContent[i].setAttribute("class", "col-10 textArea row present")
+    }
+    else {
+        textContent[i].setAttribute("class", "col-10 textArea row future")
+    }
+}
+
+storeText ()
